960384a5722d453ac097c052f9ed203316160f8d
boyvita <vitaly_boytsov@bk.ru>
2019-04-15 02:19:12 +0300

    added smart generating

--- page.js
+++ page.js
@@ -1,89 +1,58 @@
-var body = document.getElementsByTagName("body");
-canvasElement = document.createElement("canvas");
-body.appendChild(canvasElement);
+canvas = document.getElementById("canvas");
+canvas.width = 1000;
+canvas.height = 600;
+context = canvas.getContext("2d");
+context.font = "bold 50px sans-serif";
+context.textAlign = "center";
+context.strokeStyle = "white";
 
-canvasElement.width = 1000;
-canvasElement.height = 600;
-var generateButton = document.createElement("generate");  
-generateButton.innerHTML = "generate collage";
-var saveButton = document.createElement("save");
-saveButton.innerHTML = "save";   
+var body = document.getElementsByTagName("body")[0];
 
+var generateButton = document.createElement("input");
+generateButton.value = "Generate collage";
+generateButton.id = "generate";
+generateButton.type = "button";
 body.appendChild(generateButton);
+
+var saveButton = document.createElement("input");
+saveButton.value = "Save";
+saveButton.id = "save";
+saveButton.type = "button";
 body.appendChild(saveButton);
-
-
-drawCollage(canvasElement, drawQuote);
-
-quotePromise = new Promise((resolve, reject) => {
-  $.getJSON(
-    "https://api.forismatic.com/api/1.0/?method=getQuote&lang=ru&format=jsonp&jsonp=?"
-  )
-    .done(response => {
-      resolve(response);
-    })
-    .fail(err => {
-      reject(err);
-    });
+genCollagePromise();
+genQuotePromise();
+executed = 1;
+$("#generate").click(() => {
+  if (executed) {
+		executed = 0;
+    drawCollage(drawQuote);
+		executed = 1;
+  }
 });
 
-async function drawQuote(canvasElement) {
-  context = canvasElement.getContext("2d");
-  context.font = "bold 50px sans-serif";
-  context.textAlign = "center";
-  context.strokeStyle = "white";
+$("#save").click(() => {
+  var link = document.createElement("a");
+  link.download = "collage.png";
+  link.href = canvas.toDataURL(); //"image/png").replace("image/png", "image/octet-stream"); ;
+  link.click();
+});
 
-  let maxx = canvasElement.width;
-  let maxy = canvasElement.height;
-  context.textAlign = "center";
-  let maxLineWidth = maxx * 0.8;
-  quotePromise.then(
-    result => {
-      quoteText = result.quoteText;
-      quoteAuthor = result.quoteAuthor;
-      let words = quoteText.split(" ");
-      console.log(words);
-      let lines = [];
-      let line = "";
-      for (let i = 0; i < words.length; i++) {
-        let newLine = line + words[i];
-        let newLineWidth = context.measureText(newLine).width;
-        if (newLineWidth > maxLineWidth) {
-          lines.push(line);
-          line = words[i] + " ";
-        } else {
-          line = newLine + " ";
-        }
-      }
-      lines.push(line);
-      console.log(lines);
-
-      for (let i = 0; i < lines.length; i++) {
-        let x = Math.floor(maxx / 2);
-        let y =
-          Math.floor(maxy / 2) +
-          Math.floor(50 * lines.length * (i / (lines.length - 1) - 1 / 2));
-        if (lines.length == 1) {
-          y = Math.floor(maxy / 2);
-        }
-				console.log(i / (lines.length - 1) - 1 / 2);
-				
-				 context.lineWidth = 3;
-				context.strokeText(lines[i], x, y);
-				 context.lineWidth = 1;
-				context.fillText(lines[i], x, y);
-      }
-    },
-    err => {
-      console.log(err);
-    }
-  );
+function genCollagePromise() {
+  collagePromise = new Promise(resolve => {
+    genCollage(resolve);
+  });
 }
 
-async function drawCollage(canvasElement, callback) {
+function genQuotePromise() {
+  quotePromise = new Promise(resolve => {
+    genQuote(resolve);
+  });
+}
+
+function genCollage(resolve) {
   let count = 0;
-  let maxx = canvasElement.width;
-  let maxy = canvasElement.height;
+  let maxx = canvas.width;
+  let maxy = canvas.height;
   let pics = [];
   for (let i = 0; i < 4; i++) {
     pics[i] = { img: new Image() };
@@ -96,9 +65,10 @@ async function drawCollage(canvasElement, callback) {
   pics[0].szy = pics[2].y = pics[1].szy = pics[3].y = yline;
   pics[2].szy = pics[3].szy = maxy - yline;
 
-  let context = canvasElement.getContext("2d");
+  let context = canvas.getContext("2d");
   for (i = 0; i < 4; i++) {
     let pic = pics[i];
+    pics[i].crossOrigin = "anonymous";
     pic.img.src =
       "https://source.unsplash.com/random/" +
       pic.szx +
@@ -106,41 +76,79 @@ async function drawCollage(canvasElement, callback) {
       pic.szy +
       "?sig=" +
       Math.floor(Math.random() * 10000);
+
     pic.img.onload = () => {
-      context.drawImage(pic.img, pic.x, pic.y);
       count++;
-			if (count == 4) {
-				
-        callback(canvasElement);
-        count = 0;
+      if (count == 4) {
+        resolve(pics);
       }
     };
   }
 }
 
-// function wrapText(context, text, x, y, maxWidth, lineHeight) {
-//   let words = text.split(" ");
-//   let line = "";
+function genQuote(resolve) {
+  $.getJSON(
+    "https://api.forismatic.com/api/1.0/?method=getQuote&lang=en&format=jsonp&jsonp=?"
+  ).done(response => {
+    resolve(response);
+  });
+}
 
-//   for (let n = 0; n < words.length; n++) {
-//     let testLine = line + words[n] + " ";
-//     let testWidth = context.measureText(testLine).width;
-//     if (testWidth > maxWidth) {
-//       let lineWidth = context.measureText(line).width;
-//       let indent = Math.floor((maxWidth - lineWidth) / 2);
-//       context.fillText(line, x + indent, y);
+function drawCollage(callback) {
+  context = canvas.getContext("2d");
+  collagePromise.then(pics => {
+    for (i = 0; i < 4; i++) {
+      let pic = pics[i];
+      context.drawImage(pic.img, pic.x, pic.y);
+    }
+    genCollagePromise();
+    if (callback) {
+      callback();
+    }
+    executed = 1;
+  });
+}
 
-//       line = words[n] + " ";
-//       y += lineHeight;
-//     } else {
-//       line = testLine;
-//     }
-//   }
+function drawQuote(callback) {
+  let context = canvas.getContext("2d");
+  let maxx = canvas.width;
+  let maxy = canvas.height;
+  let maxLineWidth = maxx * 0.8;
+  quotePromise.then(result => {
+    quoteText = result.quoteText;
+    quoteAuthor = result.quoteAuthor;
+    let words = quoteText.split(" ");
+    console.log(words);
+    let lines = [];
+    let line = "";
+    for (let i = 0; i < words.length; i++) {
+      let newLine = line + words[i];
+      let newLineWidth = context.measureText(newLine).width;
+      if (newLineWidth > maxLineWidth) {
+        lines.push(line);
+        line = words[i] + " ";
+      } else {
+        line = newLine + " ";
+      }
+    }
+    lines.push(line);
+    console.log(lines);
 
-//   let lineWidth = context.measureText(line).width;
-//   let indent = Math.floor((maxWidth - lineWidth) / 2);
-//   context.fillText(line, x + indent, y);
-// }
+    for (let i = 0; i < lines.length; i++) {
+      let x = Math.floor(maxx / 2);
+      let y =
+        Math.floor(maxy / 2) +
+        Math.floor(50 * lines.length * (i / (lines.length - 1) - 1 / 2));
+      if (lines.length == 1) {
+        y = Math.floor(maxy / 2);
+      }
+      console.log(i / (lines.length - 1) - 1 / 2);
 
-// // ctx.font = "30px Arial";
-// // ctx.fillStyle = "black";
+      context.lineWidth = 3;
+      context.strokeText(lines[i], x, y);
+      context.lineWidth = 1;
+      context.fillText(lines[i], x, y);
+    }
+    genQuotePromise();
+  });
+}
